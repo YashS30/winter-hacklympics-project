51b02f4196c1495e71263f92f2c51c6de461f0a6
Callum Irving <callum.irving04@gmail.com>
2020-12-18 10:55:23 -0500

    Fixed spaces instead of tabs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('home.html')
+	return render_template('home.html')
 
 @app.route('/languages')
 def languages_page():
