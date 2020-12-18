66b00de5201fde40df683768fcacb750cac9f544
Callum Irving <callum.irving04@gmail.com>
2020-12-18 12:29:37 -0500

    Added some navbar styling

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ def languages_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
 	if request.method == 'POST':
+		# When the form is submitted, redirect to the home page
 		return redirect(url_for('home_page'))
 	else:
+		# Otherwise, show the login form
 		return render_template('login.html')
 
 if __name__ == "__main__":
