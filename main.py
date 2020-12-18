69df4f783f35d32dcf91a46e4f4e5c8fe09703a1
Callum Irving <callum.irving04@gmail.com>
2020-12-18 10:53:56 -0500

    Added new pages

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template("home.html")
+    return render_template('home.html')
+
+@app.route('/languages')
+def languages_page():
+	return render_template('languages.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+	if request.method == 'POST':
+		return redirect(url_for('home_page'))
+	else:
+		return render_template('login.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
