2ec3ecf7ca3a305c9ded018c0d88c73b98d9f41a
Callum Irving <callum.irving04@gmail.com>
2020-12-19 14:56:14 -0500

    Removed login

--- app.py
+++ app.py
@@ -15,15 +15,6 @@ def about_page():
 def languages_page():
 	return render_template('language_list.html', languages=languages)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login_page():
-	if request.method == 'POST':
-		# When the form is submitted, redirect to the home page
-		return redirect(url_for('home_page'))
-	else:
-		# Otherwise, show the login form
-		return render_template('login.html')
-
 @app.route('/languages/<lang>', methods=['GET'])
 def language_page(lang):
 	# Checks if the url language is in our list of languages
